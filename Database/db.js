9733f8bd847a5d96e82bdc3841f5057102e1584f
Amir Afzali <amir@afzali.ca>
2021-04-11 19:04:40 -0400

    Remove old key from db.js

--- Database/db.js
+++ Database/db.js
@@ -1,12 +1,4 @@
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyAI3uDiK0AuFj7_s-Q1tBQZdeCbUKVW-xo",
-    authDomain: "idlearcade-c04ec.firebaseapp.com",
-    projectId: "idlearcade-c04ec",
-    storageBucket: "idlearcade-c04ec.appspot.com",
-    messagingSenderId: "948764153496",
-    appId: "1:948764153496:web:6f06f01ee5877928c2e2ed"
-};
+
 // Initialize Firebase
 firebase.default.initializeApp(firebaseConfig);
 
