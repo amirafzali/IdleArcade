198758b40ce353ae1783fa751a74c06bdb5ebce0
Amir Afzali <amir@afzali.ca>
2021-04-11 04:42:20 -0400

    Add variable binding to the input forms

--- Login/auth.js
+++ Login/auth.js
@@ -3,23 +3,91 @@ $(document).ready(() => {
     $("#registerForm").hide()
     $("#resetForm").hide()
 
+    let email = "";
+    let password = "";
+    let username = "";
+    let confirm = "";
+
+    resetFields()
+    setupRedirects()
+    inputChanges()
+    formSubmits()
 
 
-    $("#registerButton").click((e) => {
-        e.preventDefault();
-        $("#loginForm").hide()
-        $("#registerForm").show()
-    })
+    function resetFields() {
+        let allFields = ['loginEmail', 'registerEmail', 'resetEmail', 'loginPassword', 'registerPassword', 'registerUsername', 'registerConfirm']
+        allFields.forEach(id => {
+            $("#"+id).val("")
+        })
+        email = ""
+        password = ""
+        username = ""
+        confirm = ""
+    }
+    
+    function setupRedirects() {
+        const gotoLogin = () => {
+            resetFields()
+            $("#loginForm").show()
+            $("#registerForm").hide()
+            $("#resetForm").hide()
+            
+        }
+    
+        $("#registerButton").click((e) => {
+            e.preventDefault();
+            resetFields()
+            $("#loginForm").hide()
+            $("#registerForm").show()
+        })
+    
+        $(".backToLogin").click((e) => {
+            e.preventDefault();
+            gotoLogin()
+        })
+    
+        $("#resetPasswordButton").click((e) => {
+            e.preventDefault();
+            resetFields()
+            $("#loginForm").hide()
+            $("#resetForm").show()
+        })
+    }
 
-    $("#backToLogin").click((e) => {
-        e.preventDefault();
-        $("#registerForm").hide()
-        $("#loginForm").show()
-    })
+    function inputChanges() {
+        let emailForms = ['loginEmail', 'registerEmail', 'resetEmail']
+        let passwordForms = ['loginPassword', 'registerPassword']
+    
+        emailForms.forEach(id => {
+            $("#"+id).bind('change paste keyup', () => {
+                email = $("#"+id).val() 
+            })
+        })
+    
+        passwordForms.forEach(id => {
+            $("#"+id).bind('change paste keyup', () => {
+                password = $("#"+id).val() 
+            })
+        })
+    
+        $("#registerUsername").bind('change paste keyup', () => {
+            username = $("#registerUsername").val() 
+        })
+    
+        $("#registerConfirm").bind('change paste keyup', () => {
+            confirm = $("#registerConfirm").val() 
+        })
+    }
 
-    $("#resetPasswordButton").click((e) => {
-        e.preventDefault();
-        $("#loginForm").hide()
-        $("#resetForm").show()
-    })
+    function formSubmits() {
+        let x = ['loginButton', 'registerFormButton', 'resetButton']
+        x.forEach((tag) => {
+            $("#"+tag).click(() => {
+                console.log(email)
+                console.log(password)
+                console.log(confirm)
+                console.log(username)
+            })
+        });
+    }
 })
