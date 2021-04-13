cce0a4e7b8915a8072aa88701c48cec0ba8cf1ef
collinkan <collin.kan@gmail.com>
2021-04-13 16:00:39 -0400

    finish wheel

--- FortuneWheel/wheel.js
+++ FortuneWheel/wheel.js
@@ -5,31 +5,33 @@ $(document).ready(() => {
   let r = Math.min(w, h)/2;
   let rotation = 0;
   let oldrotation = 0;
+  let spinsLeft = 3;
+  let winnings = 0;
 
   const prize_data = [
-    {prize:"500 Tickets", message:""},
-    {prize:"Prize 2", message:""},
-    {prize:"Prize 3", message:""},
-    {prize:"Prize 4", message:""}, 
-    {prize:"Prize 5", message:""},
-    {prize:"500 Tickets", message:""},
-    {prize:"Prize 7", message:""},
-    {prize:"Prize 8", message:""},
-    {prize:"Prize 9", message:""},
-    {prize:"Prize 10", message:""},
-    {prize:"500 Tickets", message:""},
-    {prize:"Prize 12", message:""},
-    {prize:"Prize 13", message:""},
-    {prize:"Prize 14", message:""},
-    {prize:"Prize 15", message:""},
-    {prize:"500 Tickets", message:""},
-    {prize:"Prize 17", message:""},
-    {prize:"Prize 18", message:""},
-    {prize:"Prize 19", message:""},
-    {prize:"Prize 20", message:""}
+    {prize:"50 Tickets", message:""},
+    {prize:"75 Tickets", message:""},
+    {prize:"3 More Spins", message:""},
+    {prize:"50 Tickets", message:""}, 
+    {prize:"75 Tickets", message:""},
+    {prize:"50 Tickets", message:""},
+    {prize:"75 Tickets", message:""},
+    {prize:"1,000 Tickets", message:""},
+    {prize:"50 Tickets", message:""},
+    {prize:"75 Tickets", message:""},
+    {prize:"50 Tickets", message:""},
+    {prize:"75 Tickets", message:""},
+    {prize:"3 More Spins", message:""},
+    {prize:"50 Tickets", message:""},
+    {prize:"75 Tickets", message:""},
+    {prize:"50 Tickets", message:""},
+    {prize:"75 Tickets", message:""},
+    {prize:"10,000 Tickets", message:""},
+    {prize:"50 Tickets", message:""},
+    {prize:"75 Tickets", message:""}
   ];
-  const cat1 = new Set([0, 5, 10, 15])
-  const cat2 = new Set([1, 4, 6, 9, 11, 14, 16, 19])
+  const cat1 = new Set([0, 3, 5, 8, 10, 13, 15, 18])
+  const cat2 = new Set([2, 7, 12, 17])
 
   var svg = d3.select('.wheel')
       .append("svg")
@@ -51,80 +53,96 @@ $(document).ready(() => {
 
   // select paths, use arc generator to draw
   var arcs = vis.selectAll("g.slice")
-      .data(pie)
-      .enter()
-      .append("g")
-      .attr("class", "slice");
+    .data(pie)
+    .enter()
+    .append("g")
+    .attr("class", "slice");
       
   arcs.append("path")
-      .attr("fill", function(d, i){ 
-        if (cat1.has(i)) return "#FF0000";
-        else if (cat2.has(i)) return "#BBBBBB";
-        else return "#FF0000";
-      })
-      .attr("d", function (d) { return arc(d); });
+    .attr("fill", function(d, i){ 
+      if (cat1.has(i)) return "#C3B1E1";
+      else if (cat2.has(i)) return "#DDD000";
+      else return "#00BBBB"
+    })
+    .attr("d", function (d) { return arc(d); });
 
   // add the text
   arcs.append("text").attr("transform", function(d){
-          d.innerRadius = 0;
-          d.outerRadius = r;
-          d.angle = (d.startAngle + d.endAngle)/2;
-          return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")translate(" + (d.outerRadius -10) +")";
-      })
-      .attr("text-anchor", "end")
-      .text(function(d, i) {
-          return prize_data[i].prize;
-      });
+      d.innerRadius = 0;
+      d.outerRadius = r;
+      d.angle = (d.startAngle + d.endAngle)/2;
+      return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")translate(" + (d.outerRadius -10) +")";
+    })
+    .attr("text-anchor", "end")
+    .text(function(d, i) {
+        return prize_data[i].prize;
+    });
 
-  container.on("click", spin);
+  container.on("click", () => {
+    spin();
+    document.getElementById("spins").innerHTML = spinsLeft;
+  });
 
   function spin(){
-      container.on("click", null);
+    container.on("click", null);
 
-      let ps = 360/prize_data.length
-      let rng = Math.floor((Math.random() * 5000) + 360);
-          
-      rotation = (Math.round(rng / ps) * ps);
-      
-      picked = Math.round(prize_data.length - (rotation % 360)/ps);
-      picked = picked >= prize_data.length ? (picked % prize_data.length) : picked;
+    spinsLeft--;
 
-      rotation += 90 - Math.round(ps/2);
+    let ps = 360/prize_data.length
+    let rng = Math.floor((Math.random() * 5000) + 360);
+        
+    rotation = (Math.round(rng / ps) * ps);
+    
+    picked = Math.round(prize_data.length - (rotation % 360)/ps);
+    picked = picked >= prize_data.length ? (picked % prize_data.length) : picked;
 
-      vis.transition()
-        .duration(3000)
-        .attrTween("transform", rotTween)
-        .each("end", function(){
-          //populate question
-          d3.select(".question")
-              .text("You won " + prize_data[picked].prize + "!");
+    rotation += 90 - Math.round(ps/2);
 
-          oldrotation = rotation;
-          container.on("click", spin);
+    vis.transition()
+      .duration(3000)
+      .attrTween("transform", rotTween)
+      .each("end", function(){
+        //Get prize
+        let prizeWon = prize_data[picked].prize;
+        d3.select(".prize")
+            .text("You won " + prizeWon + "!");
+        getPrize(prizeWon)
+        oldrotation = rotation;
+        if (spinsLeft === 0) {
+          container.on('click', null);
+          d3.select(".header").text("No more spins left!")
+          addTickets(winnings)
+          //CLOSE WINDOW
+          return;
+        }
+        else container.on("click", () => {
+          spin();
+          document.getElementById("spins").innerHTML = spinsLeft;
         });
+      });
   }
 
   //make arrow
   svg.append("g")
-      .attr("transform", "translate(" + (padding.left + w + padding.right/1.2) + "," + (h/2 + padding.top) + ")")
-      .append("path")
-      .attr("d", "M-" + (r*.15) + ",0L0," + (r*.05) + "L0,-" + (r*.05) + "Z")
-      .style({"fill":"#BBBBBB"});
+    .attr("transform", "translate(" + (padding.left + w + padding.right/1.2) + "," + (h/2 + padding.top) + ")")
+    .append("path")
+    .attr("d", "M-" + (r*.15) + ",0L0," + (r*.05) + "L0,-" + (r*.05) + "Z")
+    .style({"fill":"#BBBBBB"});
 
   //draw spin circle
   container.append("circle")
-      .attr("cx", 0)
-      .attr("cy", 0)
-      .attr("r", 60)
-      .style({"fill":"black","cursor":"pointer"});
+    .attr("cx", 0)
+    .attr("cy", 0)
+    .attr("r", 60)
+    .style({"fill":"black","cursor":"pointer"});
 
   //spin text
   container.append("text")
-      .attr("x", 0)
-      .attr("y", 15)
-      .attr("text-anchor", "middle")
-      .text("SPIN")
-      .style({"fill":"#BBBBBB", "font-weight":"bold", "font-size":"25px"});
+    .attr("x", 0)
+    .attr("y", 15)
+    .attr("text-anchor", "middle")
+    .text("SPIN")
+    .style({"fill":"#BBBBBB", "font-weight":"bold", "font-size":"25px"});
 
   function rotTween() {
     var i = d3.interpolate(oldrotation % 360, rotation);
@@ -132,4 +150,18 @@ $(document).ready(() => {
       return "rotate(" + i(t) + ")";
     };
   }
+
+  const getPrize = (prize) => {
+    if (prize.endsWith("Tickets")) {
+      let amount = Number(prize.split(" ")[0].replace(/,/g, ""));
+      winnings += amount
+      d3.select(".total").style({"display":"block"})
+      document.getElementById("winnings").innerHTML = winnings.toLocaleString();
+    }
+    else {
+      spinsLeft += 3;
+      document.getElementById("spins").innerHTML = spinsLeft;
+    }
+  };
+
 });
